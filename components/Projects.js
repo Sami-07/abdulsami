519e25c266b2878a979a9638c9ee736cc1ce3bd1
sami <s.a.sami359359@gmail.com>
2023-11-13 22:57:31 +0530

    animation changes

--- components/Projects.js
+++ components/Projects.js
@@ -24,22 +24,22 @@ function Projects() {
                                     <Image src={"/images/lappy.png"} className="w-[380px] max-w-[1000px]" width={600} height={600} alt="laptop" />
                                 </div>
                                 <div className='flex justify-center items-center gap-5'>
-                                    <Link  data-aos="fade-up" href={project.github} className='hover:-translate-y-2 ease-in-out transition hover:scale-105 flex  justify-center items-center  gap-2  bg-white text-black font-medium text-lg md:text-2xl p-2 rounded-md px-3'><AiOutlineGithub />Repository</Link>
-                                    <Link  data-aos="fade-up" href={project.webapp} className='hover:-translate-y-2 ease-in-out transition hover:scale-105 flex  justify-center items-center  gap-2 bg-myprimecolor font-medium text-white  text-lg md:text-2xl p-2 rounded-md px-3'><HiExternalLink />Web App</Link>
+                                    <Link  data-aos="fade-right" href={project.github} className='hover:-translate-y-2 ease-in-out transition hover:scale-105 flex  justify-center items-center  gap-2  bg-white text-black font-medium text-lg md:text-2xl p-2 rounded-md px-3'><AiOutlineGithub />Repository</Link>
+                                    <Link  data-aos="fade-left" href={project.webapp} className='hover:-translate-y-2 ease-in-out transition hover:scale-105 flex  justify-center items-center  gap-2 bg-myprimecolor font-medium text-white  text-lg md:text-2xl p-2 rounded-md px-3'><HiExternalLink />Web App</Link>
                                 </div>
                             </div>
                             <div  data-aos="fade-up" className="flex md:text-lg text-base text-left flex-col    lg:w-1/2 mt-10 md:mt-0">
                                 <h1 className=" text-xl text-left  md:text-left md:mt-5  md:text-3xl mb-3 md:mb-0 ">{project.title}</h1>
                                 <p className="text-base md:text-lg text-left">{project.desc}</p>
                                 <h3 className="text-base md:text-xl  mt-5">Tech <span className="underline decoration-myprimecolor underline-offset-4 decoration-4 rounded-lg">Stack</span>  Used:</h3>
-                                <div className="flex flex-wrap ">
+                                <div  className="flex flex-wrap ">
                                     {project.techUsed.map((tech, index) => {
                                         if (index != project.techUsed.length - 1) {
                                             tech = tech + " · "
 
                                         }
                                         return (
-                                            <p  data-aos="fade-up" key={tech} className="mr-1 text-base">{tech}</p>
+                                            <p key={tech} className="mr-1 text-base">{tech}</p>
                                         )
                                     })}
                                 </div>
